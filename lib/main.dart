e33a787d9869f9c554d692a3643ea6c5a9e7d2fc
Akaiso <akaisobenson@yahoo.com>
2024-05-03 20:10:23 +0100

    installed GetX, bg image and page transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:zakat_jahada/view/SplashScreen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(apiKey: 'key', appId: "id", messagingSenderId: 'sendid', projectId: 'myapp')
-  );
+      options: const FirebaseOptions(
+          apiKey: 'key',
+          appId: "id",
+          messagingSenderId: 'sendid',
+          projectId: 'myapp'));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'zakat jahada',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const SplashScreen(),
+      home:  const SplashScreen(),
     );
   }
 }
-
-
--- lib/view/LandingPage.dart
+++ lib/view/LandingPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:zakat_jahada/view/HomePage.dart';
 import 'package:zakat_jahada/view/LogInPage.dart';
 import 'package:zakat_jahada/view/SignUpPage.dart';
@@ -10,53 +11,90 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          child:
-         MediaQuery.of(context).size.width > 500
-            ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: const AssetImage("assets/images/bg1.jpg"),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.05), BlendMode.dstATop))),
+            // color: Colors.black,
+            child: MediaQuery.of(context).size.width > 500
+                ? Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      accessButton(() {}, "Log in"),
-                      const SizedBox(width: 15,),
-                      accessButton(() {}, "Guest"),
-                      const SizedBox(width: 15,),
-                      accessButton(() {}, "Sign up"),
-                    ]),
-              ],
-            )
-            : Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      accessButton(() {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const LogInPage()));
-                      }, "Log in"),const SizedBox(height: 25,),
-                      accessButton(() {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomePage()));
-                      }, "Guest"),const SizedBox(height: 25,),
-                      accessButton(() {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SignUpPage()));
-                      }, "Sign up"),
-                    ]),
-              )));
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            accessButton(() {
+                              Get.to(const LogInPage(),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(milliseconds: 1500));
+                            }, "Log in", Icons.login),
+                            const SizedBox(
+                              width: 55,
+                            ),
+                            accessButton(() {
+                              Get.to(HomePage(),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(milliseconds: 1500),
+                                  curve: Curves.easeInOut);
+                            }, "Guest", Icons.person),
+                            const SizedBox(
+                              width: 55,
+                            ),
+                            accessButton(() {
+                              Get.to(const SignUpPage(),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(milliseconds: 1500));
+                            }, "Sign up", Icons.add),
+                          ]),
+                    ],
+                  )
+                : Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 180.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            accessButton(() {
+                              Get.to(const LogInPage(),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(milliseconds: 1500));
+                            }, "Log in", Icons.login),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            accessButton(() {
+                              Get.to(HomePage(),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(milliseconds: 1500),
+                                  curve: Curves.easeInOut);
+                            }, "Guest", Icons.person),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            accessButton(() {
+                              Get.to(const SignUpPage(),
+                                  transition: Transition.rightToLeft,
+                                  duration: const Duration(milliseconds: 1500));
+                            }, "Sign up", Icons.add),
+                          ]),
+                    ),
+                  )));
   }
 }
 
-Widget accessButton(onPressed, text) {
+Widget accessButton(onPressed, text, icon) {
   return Column(
     children: [
-      ElevatedButton(onPressed: onPressed, child: Text(text)),
+      ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black, foregroundColor: Colors.white),
+        child:  Row(
+          children: [Icon(icon), const SizedBox(width: 10), Text(text)],
+        ),
+      ),
       //Text(text, style: const TextStyle(fontWeight: FontWeight.w500),)
     ],
   );
